767d1e1dfe0eb4ad06d8f21e355d77058c2093ac
bokyo <yung12320@daum.net>
2019-11-29 15:54:31 +0900

    .

--- DeliveryFinder/Form1.cs
+++ DeliveryFinder/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
+using System.Net;
+using System.Xml;
 
 namespace DeliveryFinder
 {
     public partial class Form1 : Form
     {
+        RestClient restClient = new RestClient(Program.baseUrl);
+        Dictionary<string, string> dataList;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var request = new RestRequest("/api/v1/companylist", Method.GET);
+
+            request.AddQueryParameter("t_key", Program.key);
+
+            //request.AddHeader("Accept", "application/json;charset=UTF-8");
+
+            var response = restClient.Execute(request);
+
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                label1.Text = response.Content;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(response.Content);
+
+            dataList = doc["Companies"].ChildNodes.Cast<XmlNode>().Select(s => new KeyValuePair<string, string>(s["Name"].InnerText, s["Code"].InnerText)).ToDictionary(s=>s.Key, s=>s.Value);
+            comboBox1.Items.AddRange(dataList.Select(x => x.Key).ToArray());
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var request = new RestRequest("/api/v1/trackingInfo", Method.GET);
+
+            request.AddQueryParameter("t_key", Program.key);
+            request.AddQueryParameter("t_code", dataList[comboBox1.Text].ToString());
+            request.AddQueryParameter("t_invoice", textBox1.Text);
+
+            var response = restClient.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                label2.Text = response.Content;
+            }
         }
     }
 }
--- DeliveryFinder/Program.cs
+++ DeliveryFinder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,22 @@ namespace DeliveryFinder
 {
     static class Program
     {
+        public const string key = "azF5t0FmueFbyG1MLGVN2g";
+        public const string baseUrl = "https://info.sweettracker.co.kr";
         /// <summary>
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+
+
     }
 }
