d5851facda52cef6176eb1d065e9e71969257734
정보교 <yung12320@daum.net>
2019-11-29 17:40:14 +0900

    .

--- DeliveryFinder/Form1.cs
+++ DeliveryFinder/Form1.cs
@@ -16,50 +16,67 @@ namespace DeliveryFinder
     public partial class Form1 : Form
     {
         RestClient restClient = new RestClient(Program.baseUrl);
-        Dictionary<string, string> dataList;
+        Dictionary<string, string> DataList;
+        List<string> SearchDataList;
+        //List<Dictionary<string, string>> SearchDataList;
 
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var request = new RestRequest("/api/v1/companylist", Method.GET);
-
-            request.AddQueryParameter("t_key", Program.key);
-
-            //request.AddHeader("Accept", "application/json;charset=UTF-8");
-
-            var response = restClient.Execute(request);
-
-            if(response.StatusCode == HttpStatusCode.OK)
-            {
-                label1.Text = response.Content;
-            }
-
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(response.Content);
-
-            dataList = doc["Companies"].ChildNodes.Cast<XmlNode>().Select(s => new KeyValuePair<string, string>(s["Name"].InnerText, s["Code"].InnerText)).ToDictionary(s=>s.Key, s=>s.Value);
-            comboBox1.Items.AddRange(dataList.Select(x => x.Key).ToArray());
-
-            comboBox1.SelectedIndex = 0;
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
             var request = new RestRequest("/api/v1/trackingInfo", Method.GET);
 
             request.AddQueryParameter("t_key", Program.key);
-            request.AddQueryParameter("t_code", dataList[comboBox1.Text].ToString());
+            request.AddQueryParameter("t_code", DataList[comboBox1.Text].ToString());
             request.AddQueryParameter("t_invoice", textBox1.Text);
 
             var response = restClient.Execute(request);
 
             if (response.StatusCode == HttpStatusCode.OK)
             {
-                label2.Text = response.Content;
+                //textBox2.Text = response.Content;
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(response.Content);
+
+                //XmlNodeList xmlNodeList = doc.GetElementsByTagName("tracking_details");
+
+                //foreach(XmlNode i in xmlNodeList)
+                //{
+
+                //}
+                XmlElement elements;
+                elements = doc["tracking_info"];
+                foreach (XmlNode i in elements)
+                {
+                    textBox2.Text += i.InnerText + "\r\n";
+                }
+            }
+            else
+                Console.WriteLine("잘못된 정보");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var request = new RestRequest("/api/v1/companylist", Method.GET);
+
+            request.AddQueryParameter("t_key", Program.key);
+
+            //request.AddHeader("Accept", "application/json;charset=UTF-8");           
+
+            var response = restClient.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(response.Content);
+
+                DataList = doc["Companies"].ChildNodes.Cast<XmlNode>().Select(s => new KeyValuePair<string, string>(s["Name"].InnerText, s["Code"].InnerText)).ToDictionary(s => s.Key, s => s.Value);
+                comboBox1.Items.AddRange(DataList.Select(x => x.Key).ToArray());
+
+                comboBox1.SelectedIndex = 0;
             }
         }
     }
